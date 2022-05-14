3c7e375e68f09e2807c932b4d2ad7832d5a36b91
Davetto <joshrgarb@gmail.com>
2022-05-14 02:55:41 -0500

    MyCommit

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import plotly.express as px
 
 DATA_URL = (
-"data/Motor_Vehicle_Collisions_Crashes.csv"
+"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.title("Motor Vehile Collisions in New York City")
@@ -13,17 +14,14 @@ st.markdown("This application is a Streamlit dashboard that can be used "
 
 @st.cache(persist=True)
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    try:
-        data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-    except KeyError:
-        pass
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
     return data
 
-data = load_data(80_000)
+data = load_data(100000)
 original_data = data
 
 st.header("Where are the most people Injured in NYC?")
@@ -60,6 +58,15 @@ st.write(pdk.Deck(
     ],
 ))
 
+st.subheader("Breakdown by Minute between %i:00 and %i:00" % (hour, (hour + 1) %24))
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour + 1))
+]
+histogram = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes': histogram})
+figure = px.bar(chart_data, x = 'minute', y = 'crashes', hover_data = ['minute', 'crashes'], height = 400)
+st.write(figure)
+
 st.header("Top 5 Dangerous Streets by Affected Type")
 select = st.selectbox('Affected Type of People', ['Pedestrians', 'Cyclists', 'Motorists'])
 
@@ -69,6 +76,8 @@ elif select == 'Cyclists':
     st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by = ["injured_cyclists"], ascending = False).dropna(how = 'any')[:5])
 else:
     st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by = ["injured_motorists"], ascending = False).dropna(how = 'any')[:5])
+
+
 
 
 if st.checkbox("Show Raw Data", False):
